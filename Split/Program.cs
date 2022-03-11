6f2510942acfee92ce68b8c0d60700a6e700709d
Sta2yan <s2_stas@mail.ru>
2022-03-11 16:47:44 +0300

    Добавьте файлы проекта.

---
+++ Split/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startText = "Hello, World !!!";
+            string[] startTextArray = startText.Split(' ');
+
+            foreach (var word in startTextArray)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
